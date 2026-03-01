a27452e66299faf1bf94d06f9017e0e1c24c8d82
chdals2995 <chdals2995@gmail.com>
2026-03-01 14:54:44 +0900

    헤더 제목까지 완성

--- src/feature/Header.tsx
+++ src/feature/Header.tsx
@@ -20,28 +20,27 @@ export default function Header({
     const section = headerSectionConfig[currentSection];
 
     return(
-        <div className="w-160 border-red-500">
+        <div className="w-160 border-red-500 border-2 flex pl-11 pt-7 items-center">
             {/* 홈 버튼 */}
-            <button className="w-11 h-11 bg-cover bg-center"
-            style={{backgroundImage:`url(${HomeIcon})`}}>
+            <button className="w-13 h-13 cursor-pointer flex items-center justify-center">
+                <img src={HomeIcon} alt="홈" className="w-12 h-11 object-contain"/>
             </button>
 
             <div>
                 {/* 제목 */}
                 {section.showTitle && (
-                <div className="flex items-center gap-2">
+                <div className="flex items-baseline gap-2 ml-13">
                     {section.image && (
-                    <img src={section.image} alt="제목 이미지" />
+                    <img src={section.image} alt="제목 이미지" className="h-11"/>
                     )}
-                    <h2>{section.title}</h2>
+                    <h2 className="text-5xl">{section.title}</h2>
                 </div>
                 )}
 
                 {/* 소제목 */}
                 {currentProject && (
                 <div className="flex items-center gap-2">
-                    <img src={currentProject.image} alt="소제목" 
-                    className="w-11"/>
+                    <img src={currentProject.image} alt="소제목"/>
                     <h3>{currentProject.title}</h3>
                 </div>
                 )}
